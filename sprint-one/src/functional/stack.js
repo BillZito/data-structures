7a7f835d5f30755b502053c52ca6be0aa67ac84d
Bill Zito <bill.r.zito@gmail.com>
2016-09-14 12:19:18 -0700

    Fix size, push, pop in stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function() {
   var someInstance = {};
-
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
-
+//0: a, 1:b
   someInstance.pop = function() {
+    if (counter > 0){
+     counter--;
+     var val = storage[counter];
+     return val;
+    } 
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
