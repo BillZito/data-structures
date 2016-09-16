67e93588cbff9f83e07c4e7c9f1e7af7c3db59a5
Frédéric Renken <renken@users.noreply.github.com>
2016-09-15 23:10:20 -0700

    started with linked list, renamed variables

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,74 @@
+var doublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    var node = new Node(value);
+    if (this.tail) {
+      this.tail.next = node;  
+    }
+    this.tail = node;
+    if (!this.head) {
+      this.head = node;
+    }
+  };
+
+  list.removeHead = function() {
+    if (this.head) {
+      if (!this.head.next) {
+        this.tail = this.head.next;
+      }
+      var oldHead = this.head.value;
+      this.head = this.head.next;
+    }
+    return oldHead;
+  };
+
+  list.contains = function(target) {
+    var pointer;
+    if (this.head) {
+      pointer = this.head;
+    } 
+    while (pointer) {
+      if (pointer.value === target) {
+        return true;
+      }
+      pointer = pointer.next;
+    }
+    return false;
+
+    //recursive way to do contains method
+    // var pointer;
+    // if (this.head) {
+    //       pointer = this.head;
+    //     } 
+    // // else {
+    //   pointer = this;
+    // }
+    // if (pointer) {
+    //   if (pointer.value === target) {
+    //     return true;
+    //   }
+    //   if (pointer.next) {
+    //     return list.contains.call(pointer.next, target);
+    //   }
+    // }
+    // return false;
+  };
+
+  return list;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
